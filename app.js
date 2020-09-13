3123cc47a4c53b59f3d242fef2b1c482a7eda74b
Egisto Parronchi Neto <egisto.parronchi@pushstart.com.br>
2020-09-13 20:35:13 -0300

    Some fun

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8080;
 
+app.use("/static", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(port, () => {
